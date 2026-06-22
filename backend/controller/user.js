cfbcd049038f523f48d08a4498aee26b7ca5adb0
DiviyaVenkat <diviyabtech@gmail.com>
2026-06-22 22:49:14 +0530

    fixing mail

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -115,9 +115,7 @@ userController.addContact = async(req,res) => {
 }
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
