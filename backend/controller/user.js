7fd03410d62a374cbc6cfc43bd7ef9bb4b4bf10a
DiviyaVenkat <diviyabtech@gmail.com>
2026-06-22 23:26:04 +0530

    smtp changed

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -115,14 +115,12 @@ userController.addContact = async(req,res) => {
 }
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
 });
 
 
